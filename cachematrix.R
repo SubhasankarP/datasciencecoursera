2c2a78834fde97a93b028598075d73a9d658a799
SubhasankarP <subhasankarpadhi92@gmail.com>
2020-06-01 08:11:51 +0530

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are written for Coursera Data Science : R programming
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 inv <- NULL
@@ -19,7 +18,9 @@ list(set = set,
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix function.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then cacheSolve will retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
